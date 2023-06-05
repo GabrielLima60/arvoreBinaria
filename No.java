bc066c79c78f69a557cada1060cc0a22dcd2f918
Gabriel Lima <73233318+GabrielLima60@users.noreply.github.com>
2023-06-05 15:12:47 -0300

    Add files via upload

---
+++ No.java
@@ -0,0 +1,18 @@
+package arvore;
+
+public class No {
+	private String descricao;
+	private No[] filhos;// N Filhos
+	private No pai;
+
+	public No(String descricao) {
+		this.descricao = descricao;
+		filhos = null;
+	}
+
+	public No(String descricao, No[] filhos) {
+		super();
+		this.descricao = descricao;
+		this.filhos = filhos;
+	}
+}
